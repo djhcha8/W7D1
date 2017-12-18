57dbc31745cfd6576a6a13b7f7f151ed2f2a3861
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2017-12-18 14:15:21 -0500

    finish redux cycle

--- todo_app/frontend/todo_redux.jsx
+++ todo_app/frontend/todo_redux.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import store from './store/store';
+
+import {receiveTodos, receiveTodo} from './actions/todo_actions';
+
+window.store = store;
+window.receiveTodos = receiveTodos;
+window.receiveTodo = receiveTodo;
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById('root');
